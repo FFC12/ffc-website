f9b949363c0d5e0b9a41d326802ee7be82c528d7
fatih <fatihsaika@gmail.com>
2023-06-24 16:50:41 +0300

    Deploy settings & v0.1

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# Python
+FROM python:3.11-alpine
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install git
+RUN apk add --no-cache git
+
+# Install virtualenv
+RUN pip install virtualenv
+
+# Create a virtual environment and activate it
+RUN virtualenv venv
+
+# Activate the virtual environment
+RUN source venv/bin/activate
+
+# Install poetry
+RUN pip install poetry
+
+# Install poetry dependencies
+RUN poetry export --without-hashes -f requirements.txt -o requirements.txt
+
+# Install any needed packages specified in requirements.txt
+RUN pip install -r requirements.txt
+
+# Make port 5000 available to the world outside this container
+EXPOSE 8000
+
+# Run main.py when the container launches background
+CMD ["python", "main.py", "&"]
+
+
--- main.py
+++ main.py
@@ -2,29 +2,46 @@ from fatihserver.framework.app import App, set_log_level
 from fatihserver.framework.router import HttpRouter
 from fatihserver.server.request_handler import Request, Response
 from fatihserver.framework.static_files import StaticFiles
+from fatihserver.framework.templates import Templates, TemplateResponse
 
 set_log_level('INFO')
 
+# Create router
 router = HttpRouter()
 
+# Create static files
 static = StaticFiles()
 
+# Create templates
+templates = Templates()
 
-@router.get("/index")
+
+@router.get("/")
 def index(request):
-    with open("index.html", "r") as f:
-        page = f.read()
-        return Response(status_code=200, body=page, headers={
-            "Content-Type": "text/html",
-            "Server": "FatihServer"
-        })
+    return TemplateResponse(template=templates, name="index", payload={
+        "name": "Fatih",
+        "title": "FFC | Personal Website",
+        "cv_link": "https://google.com",
+        "github_link": "https://github.com/FFC12",
+        "linkedin_link": "https://linkedin.com/in/furkanfatihcetindil",
+        "mail_link": "fatihsaika@gmail.com"
+    })
 
 
 def main():
+    # Add static files
     static.add_static_dir(directory="assets/")
+
+    # Add static route
     router.add_static_route(static)
 
-    app = App(router=router, host="localhost", port=8080)
+    # Add templates
+    templates.add_templates(path="templates/")
+
+    # Create app
+    app = App(router=router, host="0.0.0.0", port=8000)
+
+    # Run app
     app.run()
 
 
